f79e0492396449241be490eda8fde52072097b11
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-04-29 17:13:59 +0530

    [Varsha] : Added : Spring Boot project setup with layered architecture

---
+++ src/main/java/com/fundoo/notes/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.fundoo.notes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Fundoo Notes Backend Running";
+    }
+}
