a9f905b3143216743bab0df4963b77774d86be2b
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-04-29 17:26:15 +0530

    [Varsha] : Added : UC3 : User entity and JPA repository implementation

---
+++ src/main/java/com/fundoo/notes/entity/User.java
@@ -0,0 +1,23 @@
+package com.fundoo.notes.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
---
+++ src/main/java/com/fundoo/notes/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.fundoo.notes.repository;
+
+import com.fundoo.notes.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
